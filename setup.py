8698296b779f9f9fe555d31633082c443e61fde9
admiralspark <admiralspark@gmail.com>
2017-10-29 17:27:21 -0800

    Formatting, versioning, prep for pip 2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   packages = ['FortigateApi'], # this must be the same as the name above
   version = '1.11',
   description = 'Access Fortigate REST API in python',
-  author = 'David Chayla',
+  author = 'Original author David Chayla and Pypi maintainer AdmiralSpark',
   author_email = 'null',
   url = 'https://github.com/DavidChayla/FortigateApi', 
   download_url = 'https://github.com/DavidChayla/FortigateApi/archive/0.1.tar.gz', 
