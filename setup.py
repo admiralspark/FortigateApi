0e23deae6d0d91b0f238fda22ab043b2a7f4da91
Admiralspark <admiralspark@gmail.com>
2017-12-06 11:00:27 -0900

    Fixed Setup.py...hopefully last time

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
-from distutils.core import setup
+"""setup.py"""
+from setuptools import setup
+from codecs import open as codecs_open
+
+with codecs_open('README.md', 'r', 'utf-8') as f:
+    README = f.read()
+
 setup(
-  name = 'FortigateApi',
-  packages = ['FortigateApi'], # this must be the same as the name above
-  version = '1.11',
-  description = 'Access Fortigate REST API in python',
-  author = 'Original author David Chayla and Pypi maintainer AdmiralSpark',
-  author_email = 'null',
-  url = 'https://github.com/admiralspark/FortigateApi', 
-  download_url = 'https://github.com/admiralspark/FortigateApi/archive/1.11.tar.gz', 
-  keywords = ['fortigate', 'REST', 'network'],
-  classifiers = [],
+     # Application name:
+    name='FortigateApi',
+
+    # Version number:
+    version='1.11',
+    
+    # Description:
+    description='Access Fortigate REST API in python',
+    long_description=open("README.rst").read(),
+    
+    # Application author details:
+    author='Original author David Chayla and Pypi maintainer AdmiralSpark',
+    author_email='null',
+    
+    # Details:
+    url='https://github.com/admiralspark/FortigateApi',
+    download_url='https://github.com/admiralspark/FortigateApi/archive/1.11.tar.gz', 
+
+    # License
+    license='GPL-3.0',
+    
+    # Packages:
+    packages=['FortigateApi'],
+    
+    # Searchables:
+    keywords=['fortigate', 'REST', 'network'],
 )
